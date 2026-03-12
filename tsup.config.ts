1b2c86042402ad92256bcb0861eb26f3a8d2e3ba
grind-t <grindt.gd@gmail.com>
2026-03-12 22:49:59 +0300

    corrections

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   name: "@grind-t/lib",
-  entry: ["lib/index.ts"],
+  entry: ["src/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
   experimentalDts: true,
